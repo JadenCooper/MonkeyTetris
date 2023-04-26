1bfd4bfbeefccaf0f787f2bb850e8b6d61e4b731
JadenCooper <83618082+JadenCooper@users.noreply.github.com>
2023-04-27 09:56:01 +1200

    Create Data Dictionary

--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public enum Tetromino
@@ -10,9 +13,15 @@ public enum Tetromino
     S,
     Z
 }
-
+[System.Serializable]
 public struct TetrominoData
 {
     public Tetromino tetromino;
     public Tile tile;
+    public Vector2Int[] cells;
+
+    public void Initalize()
+    {
+        this.cells = Data.Cells[this.tetromino];
+    }
 }
