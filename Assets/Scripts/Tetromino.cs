44eaa27b8a056e14fe79261bde82e82b3b4703ea
JadenCooper <83618082+JadenCooper@users.noreply.github.com>
2023-04-27 09:40:39 +1200

    Created Basic Tetromino Data And Enum

---
+++ Assets/Scripts/Tetromino.cs
@@ -0,0 +1,18 @@
+using UnityEngine.Tilemaps;
+
+public enum Tetromino
+{
+    I,
+    O,
+    T,
+    J,
+    L,
+    S,
+    Z
+}
+
+public struct TetrominoData
+{
+    public Tetromino tetromino;
+    public Tile tile;
+}
