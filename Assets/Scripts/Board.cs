873b02dd7a22a45f250b4b233ad1a1891e59ffb3
JadenCooper <83618082+JadenCooper@users.noreply.github.com>
2023-04-27 09:59:52 +1200

    Initalized Tetrominos

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,8 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Board : MonoBehaviour
 {
+    public Tilemap tilemap;
     public TetrominoData[] tetrominos;
+
+    private void Awake()
+    {
+        tilemap = GetComponentInChildren<Tilemap>();
+
+        for (int i = 0; i < tetrominos.Length; i++)
+        {
+            tetrominos[i].Initalize();
+        }
+    }
+
+    private void Start()
+    {
+        
+    }
+
 }
