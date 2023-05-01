c35fedc0adf9eea4a8bc8daff092b0a122f2e02b
JadenCooper <jaden_cooper@outlook.com>
2023-05-01 20:35:54 +1200

    Created Piece

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -6,11 +6,13 @@ using UnityEngine.Tilemaps;
 public class Board : MonoBehaviour
 {
     public Tilemap tilemap;
+    public Piece activePiece;
     public TetrominoData[] tetrominos;
-
+    public Vector3Int spawnPosition;
     private void Awake()
     {
         tilemap = GetComponentInChildren<Tilemap>();
+        activePiece = GetComponentInChildren<Piece>();
 
         for (int i = 0; i < tetrominos.Length; i++)
         {
@@ -20,7 +22,24 @@ public class Board : MonoBehaviour
 
     private void Start()
     {
-        
+        SpawnPiece();
+    }
+
+    public void SpawnPiece()
+    {
+        TetrominoData data = tetrominos[Random.Range(0, tetrominos.Length)];
+        activePiece.Initialize(this, spawnPosition , data);
+        Set(activePiece);
+    }
+
+    public void Set(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            // Add Piece Position To Get The Actual Location Compared To Local Position
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            tilemap.SetTile(tilePosition, piece.data.tile);
+        }
     }
 
 }
---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    public Board board;
+    public TetrominoData data;
+    public Vector3Int position;
+    public Vector3Int[] cells;
+    public void Initialize(Board board, Vector3Int position, TetrominoData data)
+    {
+        this.board = board;
+        this.position = position;
+        this.data = data;
+
+        if (this.cells == null)
+        {
+            // Initialize Cell Array If Not Initialized
+            cells = new Vector3Int[data.cells.Length];
+        }
+
+        for (int i = 0; i < data.cells.Length; i++)
+        {
+            cells[i] = (Vector3Int)data.cells[i];
+        }
+    }
+}
