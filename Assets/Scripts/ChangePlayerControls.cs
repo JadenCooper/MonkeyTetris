e58029928d29f954990cc1767ab8d00c6f25ebaf
JadenCooper <jaden_cooper@outlook.com>
2023-05-24 22:10:00 +1200

    Added Reset Controls Fuctionalty

--- Assets/Scripts/ChangePlayerControls.cs
+++ Assets/Scripts/ChangePlayerControls.cs
@@ -9,6 +9,7 @@ public class ChangePlayerControls : MonoBehaviour
     public List<Button> controlButtons = new List<Button>();
     public List<TMP_Text> controlTexts = new List<TMP_Text>();
     public List<PlayerControls> playerControls = new List<PlayerControls>();
+    public List<PlayerControls> playerControlsDefaults = new List<PlayerControls>();
     private bool listening = false;
     private int ControlIndex;
     private void Update()
@@ -16,6 +17,7 @@ public class ChangePlayerControls : MonoBehaviour
         if (Input.anyKeyDown && listening)
         {
             KeyCode keyCode = GetPressedKeyCode();
+            SetControl(keyCode);
             //Debug.Log("Key Pressed: " + keyCode.ToString());
         }
     }
@@ -55,5 +57,51 @@ public class ChangePlayerControls : MonoBehaviour
             controlButtons[i].interactable = !controlButtons[i].interactable;
         }
         listening = !listening;
+    }
+
+    public void SetControl(KeyCode keycode)
+    {
+        int PlayerIndex = 0;
+        if (ControlIndex >= 5)
+        {
+            ControlIndex -= 5;
+            PlayerIndex++;
+
+        }
+        switch (ControlIndex)
+        {
+            case 0:
+                playerControls[PlayerIndex].Rotate = keycode;
+                break;
+
+            case 1:
+                playerControls[PlayerIndex].MoveLeft = keycode;
+                break;
+
+            case 2:
+                playerControls[PlayerIndex].MoveRight = keycode;
+                break;
+
+            case 3:
+                playerControls[PlayerIndex].SoftDrop = keycode;
+                break;
+
+            case 4:
+                playerControls[PlayerIndex].HardDrop = keycode;
+                break;
+
+            default:
+                Debug.Log("Set Control Broke");
+                break;
+        }
+    }
+
+    public void ResetControls()
+    {
+        Debug.Log("Reset");
+        for (int i = 0; i < playerControls.Count; i++)
+        {
+            playerControls[i].CopyFrom(playerControlsDefaults[i]);
+        }
     }
 }
