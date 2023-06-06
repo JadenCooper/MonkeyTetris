670037b07d80229d798f5a25e17115184bbe980f
monty1289 <monty.wood1289@gmail.com>
2023-06-06 15:08:03 +1200

    ui added for defaulf controls

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -6,6 +6,9 @@ public class UIManager : MonoBehaviour
 {
     public TMP_Text activePlayerText;
     public TMP_Text LostPlayerText;
+    public TMP_Text Player1Controls;
+    public TMP_Text Player2Control;
+
     public void PlayerChange(int PlayerNumber)
     {
         activePlayerText.text = "Player " + PlayerNumber + " Active";
@@ -14,5 +17,10 @@ public class UIManager : MonoBehaviour
     public void GameOver(int PlayerNumber)
     {
         LostPlayerText.text = "Player " + PlayerNumber + " Lost";
+    }
+
+    public void DisplayControls()
+    {
+        
     }
 }
