6ce09b65d56f238404f2e252825d331887e9e0a3
monty1289 <monty.wood1289@gmail.com>
2023-06-08 10:05:36 +1200

    ui no shows updated player controls

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -6,8 +6,29 @@ public class UIManager : MonoBehaviour
 {
     public TMP_Text activePlayerText;
     public TMP_Text LostPlayerText;
-    public TMP_Text Player1Controls;
-    public TMP_Text Player2Control;
+    public TMP_Text Player1ControlsText;
+    public TMP_Text Player2ControlText;
+
+    public PlayerControls player1Controls;
+    public PlayerControls player2Controls;
+
+    void Start()
+    {        
+        //displays controls to the UI for player 1 and 2
+        Player1ControlsText.text = 
+        "Move Left: " + player1Controls.MoveLeft.ToString() + 
+        "\nMove right: " + player1Controls.MoveRight.ToString() + 
+        "\nRotate: " + player1Controls.Rotate.ToString() + 
+        "\nSoft Drop : " + player1Controls.SoftDrop.ToString() +
+        "\nHard Drop : " + player1Controls.HardDrop.ToString() + "\n";
+
+        Player2ControlText.text = 
+        "Move Left: " + player2Controls.MoveLeft.ToString() + 
+        "\nMove right: " + player2Controls.MoveRight.ToString() + 
+        "\nRotate: " + player2Controls.Rotate.ToString() + 
+        "\nSoft Drop : " + player2Controls.SoftDrop.ToString() +
+        "\nHard Drop : " + player2Controls.HardDrop.ToString() + "\n";        
+    }
 
     public void PlayerChange(int PlayerNumber)
     {
@@ -19,8 +40,6 @@ public class UIManager : MonoBehaviour
         LostPlayerText.text = "Player " + PlayerNumber + " Lost";
     }
 
-    public void DisplayControls()
-    {
-        
-    }
+
+
 }
