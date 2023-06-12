9eff14126c5b2e95222859a9087e022d019ec50b
JadenCooper <83618082+JadenCooper@users.noreply.github.com>
2023-06-12 13:35:17 +1200

    Revert "Closed The Game Loop"

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -8,8 +8,7 @@ public class GameManager : MonoBehaviour
     public Board board;
     public PickupManager pickupManager;
     public int bananaAmount;
-    public Vector2 CurrentScore = Vector2.zero;
-    public int ScoreGoal = 100;
+    public Vector2 Score = Vector2.zero;
     private void Awake()
     {
         UIManager = GetComponent<UIManager>();
@@ -17,53 +16,34 @@ public class GameManager : MonoBehaviour
     }
     public void GameOver(int PlayerNumber)
     {
-        Time.timeScale = 0f;
         UIManager.GameOver(PlayerNumber);
+        pickupManager.StartGame();
     }
 
-    public void SetBoardSize(int BoardSize)
+    public void PlayerChange(int PlayerNumber)
     {
-        UIManager.SetBoardSize(BoardSize, ScoreGoal, CurrentScore);
+        UIManager.PlayerChange(PlayerNumber);
     }
 
-    public void PickupCollected(string Pickup)
+    public void BananaCollected(int PlayerIndex)
     {
-        UIManager.PickupCollected(Pickup);
-    }
-
-    public void BananaCollected(int PlayerIndex, int ScoreChange)
-    {
-        PickupCollected("Banana");
         // Player Currently Gets 1 Score Per Banana
-        int Playerscore = 0;
         if (PlayerIndex == 0)
         {
             //Player One
-            CurrentScore.x += ScoreChange;
-            Playerscore = (int)CurrentScore.x;
+            Score.x++;
         }
         else
         {
             //Player Two
-            CurrentScore.y += ScoreChange;
-            Playerscore = (int)CurrentScore.y;
+            Score.y++;
         }
-
-        UIManager.SetScores(CurrentScore);
-
-        if (Playerscore >= ScoreGoal)
+        Debug.Log("Score Now Stands At Player One: " + Score.x + " Player Two: " + Score.y);
+        bananaAmount--;
+        if (bananaAmount <= 0)
         {
-            GameOver(PlayerIndex);
-        }
-        else
-        {
-            Debug.Log("Score Now Stands At Player One: " + CurrentScore.x + " Player Two: " + CurrentScore.y);
-            bananaAmount--;
-            if (bananaAmount <= 0)
-            {
-                // If All Bananas Are Collected Then Spawn Some More
-                pickupManager.SpawnBananas();
-            }
+            // If All Bananas Are Collected Then Spawn Some More
+            pickupManager.SpawnBananas();
         }
     }
 }
