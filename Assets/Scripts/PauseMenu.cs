e28d17be9848f188beb9f73e3e6439cdf8c23b30
CalebStephens <calebtaylorstephens@gmail.com>
2023-06-12 19:18:57 +1200

    comments

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -7,34 +7,36 @@ public class PauseMenu : MonoBehaviour
 {
     private bool isPaused = false;
     public GameObject pausePanel;
+public void PauseGame()
+{
+    isPaused = true;
+    pausePanel.SetActive(true); // Show the pause panel or menu
+    Time.timeScale = 0f; // Set the time scale to 0 to pause the game
+    Debug.Log("Game paused");
+}
 
-    public void PauseGame()
-    {
-        isPaused = true;
-        pausePanel.SetActive(true);
-        Time.timeScale = 0f; // Set the time scale to 0 to pause the game
-        Debug.Log("Game paused");
-    }
+public void ResumeGame()
+{
+    isPaused = false;
+    Time.timeScale = 1f; // Set the time scale back to 1 to resume the game
+    pausePanel.SetActive(false); // Hide the pause panel or menu
+}
 
-    public void ResumeGame()
-    {
-        isPaused = false;
-        Time.timeScale = 1f; // Set the time scale to 0 to pause the game
-        pausePanel.SetActive(false);
-    }
+public void Restart()
+{
+    Time.timeScale = 1f; // Set the time scale back to 1 before loading the scene
+    SceneManager.LoadScene("SinglePlayerScene"); // Load the scene named "SinglePlayerScene"
+}
 
-    public void Restart(){
-        Time.timeScale = 1f;
-        SceneManager.LoadScene("SinglePlayerScene");
-    }
+public void ExitToMenu()
+{
+    Time.timeScale = 1f; // Set the time scale back to 1 before loading the scene
+    SceneManager.LoadScene("Menu"); // Load the scene named "Menu"
+}
 
-    public void ExitToMenu(){
-        Time.timeScale = 1f;
-        SceneManager.LoadScene("Menu");
-    }
+public void ExitGame()
+{
+    Application.Quit(); // Quit the application
+}
 
-    public void ExitGame()
-    {
-        Application.Quit();
-    }
 }
